620ff9ba7cf7aa8a7e90b755be8f7ff41b5eb382
Aestheticsn <114719718+Aestheticsn@users.noreply.github.com>
2022-10-04 22:15:56 +0500

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(1);
+            double b = Convert.ToDouble(-9);
+            double c = Convert.ToDouble(14);
+            double D = Convert.ToDouble(Math.Pow(b,2)-(4*a*c));
+            double x1 = Convert.ToDouble((-b + Math.Sqrt(D)/2*a));
+            double x2 = Convert.ToDouble((-b - Math.Sqrt(D) / 2 * a));
+            
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+
+        }
+    }
+}
